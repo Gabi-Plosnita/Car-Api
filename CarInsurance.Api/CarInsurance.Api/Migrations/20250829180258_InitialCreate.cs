83f1dc333541978dda24bec23fb4126fa287b17e
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-29 21:04:15 +0300

    Create initial migration

---
+++ CarInsurance.Api/CarInsurance.Api/Migrations/20250829180258_InitialCreate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarInsurance.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			// method left empty intentionally
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			// method left empty intentionally
+		}
+	}
+}
