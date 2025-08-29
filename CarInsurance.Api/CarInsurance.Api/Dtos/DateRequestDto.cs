ad63278254c856f1ba5667b3832a0d97500e26d8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 01:14:41 +0300

    Create DateRequestDto.cs

---
+++ CarInsurance.Api/CarInsurance.Api/Dtos/DateRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarInsurance.Api.Dtos;
+
+public class DateRequestDto
+{
+	[Required]
+	[DataType(DataType.Date)]
+	public DateOnly Date { get; set; }
+}
