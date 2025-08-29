b81095132271eec1a5257d3fd4e597e693aef002
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-29 22:48:38 +0300

    Update property name in CarResponseDto

--- CarInsurance.Api/CarInsurance.Api/Dtos/CarResponseDto.cs
+++ CarInsurance.Api/CarInsurance.Api/Dtos/CarResponseDto.cs
@@ -1,4 +1,4 @@
 ﻿namespace CarInsurance.Api.Dtos;
 
-public record CarResponseDto(long Id, string Vin, string? Make, string? Model, int Year, long OwnerId, string OwnerName, string? OwnerEmail);
+public record CarResponseDto(long Id, string Vin, string? Make, string? Model, int YearOfManufacture, long OwnerId, string OwnerName, string? OwnerEmail);
 
