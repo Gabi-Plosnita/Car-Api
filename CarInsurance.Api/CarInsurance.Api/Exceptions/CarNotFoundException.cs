31e2eef591180ecb79c5d452e08310bf1c014e23
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 00:25:28 +0300

    Create CarNotFoundException.cs

---
+++ CarInsurance.Api/CarInsurance.Api/Exceptions/CarNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace CarInsurance.Api.Exceptions;
+
+public class CarNotFoundException : Exception
+{
+	public CarNotFoundException() { }
+
+	public CarNotFoundException(string message)
+		: base(message) { }
+
+	public CarNotFoundException(string message, Exception inner)
+		: base(message, inner) { }
+}
+
