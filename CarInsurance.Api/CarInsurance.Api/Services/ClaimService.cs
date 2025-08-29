ac31f4a1a807b7bfc4844904d40581df4083e53f
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 00:38:42 +0300

    Create CreateAsync method for ClaimService

--- CarInsurance.Api/CarInsurance.Api/Services/ClaimService.cs
+++ CarInsurance.Api/CarInsurance.Api/Services/ClaimService.cs
@@ -1,17 +1,22 @@
 ﻿using AutoMapper;
 using CarInsurance.Api.Data;
 using CarInsurance.Api.Dtos;
+using CarInsurance.Api.Exceptions;
 using CarInsurance.Api.Models;
 
 namespace CarInsurance.Api.Services;
 
-public class ClaimService(AppDbContext _db, IMapper _mapper, ICarValidatorService _carValidator) : IClaimService
+public class ClaimService(AppDbContext _db, 
+						  IMapper _mapper, 
+						  ICarValidatorService _carValidator,
+						  IInsuranceValidatorService _insuranceValidator) : IClaimService
 {
-	public async Task<InsuranceClaimResponseDto> CreateAsync(long carId, InsuranceClaimRequestDto requestDto)
+	public async Task<InsuranceClaimResponseDto> CreateAsync(long carId, InsuranceClaimRequestDto dto)
 	{
-		await _carValidator.ValidateCarExistance(carId);
+		await _carValidator.EnsureCarExists(carId);
+		await _insuranceValidator.EnsureIsCoveredOnDate(carId, dto.ClaimDate);
 
-		var entity = _mapper.Map<InsuranceClaim>(requestDto);
+		var entity = _mapper.Map<InsuranceClaim>(dto);
 		entity.CarId = carId;
 
 		_db.InsuranceClaims.Add(entity);
