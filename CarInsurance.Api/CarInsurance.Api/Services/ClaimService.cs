7e806b4cfc71714881b780e287f2e8390ff44bfb
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-29 23:00:46 +0300

    Create and implement IClaimService

---
+++ CarInsurance.Api/CarInsurance.Api/Services/ClaimService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarInsurance.Api.Data;
+using CarInsurance.Api.Dtos;
+using CarInsurance.Api.Models;
+
+namespace CarInsurance.Api.Services;
+
+public class ClaimService(AppDbContext _db, IMapper _mapper, ICarValidatorService _carValidator) : IClaimService
+{
+	public async Task<ClaimResponseDto> CreateAsync(long carId, ClaimRequestDto requestDto)
+	{
+		await _carValidator.ValidateCarExistance(carId);
+
+		var entity = _mapper.Map<InsuranceClaim>(requestDto);
+		entity.CarId = carId;
+
+		_db.InsuranceClaims.Add(entity);
+		await _db.SaveChangesAsync();
+
+		return _mapper.Map<ClaimResponseDto>(entity);
+	}
+}
---
+++ CarInsurance.Api/CarInsurance.Api/Services/IClaimService.cs
@@ -0,0 +1,8 @@
+﻿using CarInsurance.Api.Dtos;
+
+namespace CarInsurance.Api.Services;
+
+public interface IClaimService
+{
+	Task<ClaimResponseDto> CreateAsync(long carId, ClaimRequestDto dto);
+}
