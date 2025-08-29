5c56f2a8daf5c6207ffb6773af725cf84721c60c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-29 22:13:57 +0300

    Update IsInsuranceValidAsync method

--- CarInsurance.Api/CarInsurance.Api/Services/InsuranceService.cs
+++ CarInsurance.Api/CarInsurance.Api/Services/InsuranceService.cs
@@ -1,5 +1,4 @@
 ﻿using CarInsurance.Api.Data;
-using CarInsurance.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarInsurance.Api.Services;
@@ -9,9 +8,11 @@ public class InsuranceService(AppDbContext _db, ICarValidatorService _carValidat
 	public async Task<bool> IsInsuranceValidAsync(long carId, DateOnly date)
 	{
 		await _carValidatorService.ValidateCarExistance(carId);
-		return await _db.Policies.AnyAsync(p => p.CarId == carId && ValidatePolicyDate(p, date));
-	}
 
-	private bool ValidatePolicyDate(InsurancePolicy policy, DateOnly date)
-		=> policy.StartDate <= date && policy.EndDate >= date;
+		return await _db.Policies.AnyAsync(p =>
+			p.CarId == carId &&
+			p.StartDate <= date &&
+			p.EndDate >= date
+		);
+	}
 }
