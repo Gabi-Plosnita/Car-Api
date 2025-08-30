8091bc8d0326b355dc1ee90be08041d0633d3fa4
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 15:17:43 +0300

    Create TestBase class
    It is used to work run our tests on a database

---
+++ CarInsurance.Api/CarInsurance.Api.Test/TestBase.cs
@@ -0,0 +1,33 @@
+﻿using CarInsurance.Api.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarInsurance.Api.Test;
+
+public abstract class TestBase
+{
+	private SqliteConnection _conn = default!;
+	protected AppDbContext Db { get; private set; } = default!;
+
+	[TestInitialize]
+	public void Initialize()
+	{
+		_conn = new SqliteConnection("Data Source=:memory:");
+		_conn.Open();
+
+		var options = new DbContextOptionsBuilder<AppDbContext>()
+			.UseSqlite(_conn)
+			.Options;
+
+		Db = new AppDbContext(options);
+
+		Db.Database.EnsureCreated();
+	}
+
+	[TestCleanup]
+	public void Cleanup()
+	{
+		Db?.Dispose();
+		_conn?.Dispose(); 
+	}
+}
