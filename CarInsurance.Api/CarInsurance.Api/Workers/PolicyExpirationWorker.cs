cd53743063e7005a1c11bf00a1d1a5e034a73dc6
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 21:19:42 +0300

    Use _timeProvider for delay so we can moq it in tests

--- CarInsurance.Api/CarInsurance.Api/Workers/PolicyExpirationWorker.cs
+++ CarInsurance.Api/CarInsurance.Api/Workers/PolicyExpirationWorker.cs
@@ -3,9 +3,10 @@
 namespace CarInsurance.Api.Jobs;
 
 public class PolicyExpirationWorker(ILogger<PolicyExpirationWorker> _logger,
-										   IServiceScopeFactory _scopeFactory,
-										   IClock _clock,
-										   TimeZoneInfo _appTimeZone) : BackgroundService
+									IServiceScopeFactory _scopeFactory,
+									IClock _clock,
+									TimeZoneInfo _appTimeZone,
+									TimeProvider _timeProvider) : BackgroundService
 {
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
@@ -13,20 +14,21 @@ public class PolicyExpirationWorker(ILogger<PolicyExpirationWorker> _logger,
 		{
 			var nowUtc = _clock.UtcNow;
 			var nowLocal = TimeZoneInfo.ConvertTime(nowUtc, _appTimeZone);
+
 			/*This task run each day at 00:30. If you want to test the loging functionality on the database
 			  you can simply adjust the hour and minute and the logic will be called and expired policies will
 			  be logged.
 			 */
-			
-			var nextLocal = new DateTime(nowLocal.Year, nowLocal.Month, nowLocal.Day, 0, 30, 0); 
+			var nextLocal = new DateTime(nowLocal.Year, nowLocal.Month, nowLocal.Day, 0, 30, 0);
 			if (nowLocal >= nextLocal) nextLocal = nextLocal.AddDays(1);
+
 			var nextUtc = TimeZoneInfo.ConvertTimeToUtc(nextLocal, _appTimeZone);
 			var delay = nextUtc - nowUtc;
 
 			if (delay > TimeSpan.Zero)
 			{
 				_logger.LogInformation("PolicyExpirationWorker sleeping until {NextUtc:o}", nextUtc);
-				try { await Task.Delay(delay, stoppingToken); } catch (TaskCanceledException) { break; }
+				try { await Task.Delay(delay, _timeProvider, stoppingToken); } catch (TaskCanceledException) { break; }
 			}
 
 			try
