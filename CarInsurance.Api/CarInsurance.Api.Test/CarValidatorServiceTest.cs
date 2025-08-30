cf544239041b3c9cbdd37fdaa7a361c44ed2b4f5
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-08-30 15:57:34 +0300

    Create SeedHelper and use it in CarValidator tests

--- CarInsurance.Api/CarInsurance.Api.Test/CarValidatorServiceTest.cs
+++ CarInsurance.Api/CarInsurance.Api.Test/CarValidatorServiceTest.cs
@@ -1,6 +1,5 @@
 ﻿using AwesomeAssertions;
 using CarInsurance.Api.Exceptions;
-using CarInsurance.Api.Models;
 using CarInsurance.Api.Services;
 
 namespace CarInsurance.Api.Test;
@@ -11,7 +10,7 @@ public class CarValidatorServiceTests : TestBase
 	[TestMethod]
 	public async Task ValidateCarExistanceAsync_ReturnsTrue_WhenCarExists()
 	{
-		await AddCarAsync();
+		await SeedHelper.AddCarAsync(Db, 1);
 		var service = new CarValidatorService(Db);
 
 		var result = await service.ValidateCarExistanceAsync(1);
@@ -32,7 +31,7 @@ public class CarValidatorServiceTests : TestBase
 	[TestMethod]
 	public async Task EnsureCarExistsAsync_DoesNotThrow_WhenCarExists()
 	{
-		await AddCarAsync();
+		await SeedHelper.AddCarAsync(Db, 1);
 		var service = new CarValidatorService(Db);
 
 		Func<Task> act = async () => await service.EnsureCarExistsAsync(1);
@@ -50,17 +49,5 @@ public class CarValidatorServiceTests : TestBase
 		await act.Should()
 			.ThrowAsync<CarNotFoundException>()
 			.WithMessage("Car 42 not found");
-	}
-
-	private async Task AddCarAsync()
-	{
-		Db.Cars.Add(new Car
-		{
-			Id = 1,
-			Vin = "VIN1",
-			YearOfManufacture = 2020,
-			Owner = new Owner { Id = 10, Name = "Test Owner" }
-		});
-		await Db.SaveChangesAsync();
 	}
 }
---
+++ CarInsurance.Api/CarInsurance.Api.Test/SeedHelper.cs
@@ -0,0 +1,32 @@
+﻿using CarInsurance.Api.Data;
+using CarInsurance.Api.Models;
+
+namespace CarInsurance.Api.Test;
+
+public static class SeedHelper
+{
+	public static async Task AddCarAsync(AppDbContext db, long carId, long ownerId = 100, string ownerName = "Owner")
+	{
+		db.Cars.Add(new Car
+		{
+			Id = carId,
+			Vin = $"VIN{carId:D6}",
+			YearOfManufacture = 2020,
+			Owner = new Owner { Id = ownerId, Name = ownerName }
+		});
+		await db.SaveChangesAsync();
+	}
+
+	public static async Task AddPolicyAsync(AppDbContext db, long policyId, long carId, DateOnly start, DateOnly end, string? provider = null)
+	{
+		db.Policies.Add(new InsurancePolicy
+		{
+			Id = policyId,
+			CarId = carId,
+			StartDate = start,
+			EndDate = end,
+			Provider = provider
+		});
+		await db.SaveChangesAsync();
+	}
+}
